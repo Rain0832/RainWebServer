cf359e290839aced30bd22494c27a131d1fd0bea
Rain0832 <2710007824@qq.com>
2025-08-07 22:30:29 +0800

    refactor: Rewrite the comment for EchoServer class

--- src/main.cc
+++ src/main.cc
@@ -15,18 +15,12 @@ static const off_t kRollSize = 1 * 1024 * 1024;
 class EchoServer
 {
 public:
-    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name)
-        : server_(loop, addr, name)
-        , loop_(loop)
+    EchoServer(EventLoop *loop, const InetAddress &addr, const std::string &name) : server_(loop, addr, name), loop_(loop)
     {
-        // 注册回调函数
-        server_.setConnectionCallback(
-            std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
-        
-        server_.setMessageCallback(
-            std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
-
-        // 设置合适的subloop线程数量
+        // Register the server to the event loop(Callback function)
+        server_.setConnectionCallback(std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
+        server_.setMessageCallback(std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        // Set the number of subloop threads
         server_.setThreadNum(3);
     }
     void start()
@@ -35,29 +29,29 @@ public:
     }
 
 private:
-    // 连接建立或断开的回调函数
+    // New connection established
+    // or
+    // Existing connection broken
     void onConnection(const TcpConnectionPtr &conn)   
     {
         if (conn->connected())
-        {
-            LOG_INFO<<"Connection UP :"<<conn->peerAddress().toIpPort().c_str();
+    {
+            LOG_INFO << "Connection UP :" << conn->peerAddress().toIpPort().c_str();
         }
         else
         {
-            LOG_INFO<<"Connection DOWN :"<<conn->peerAddress().toIpPort().c_str();
+            LOG_INFO << "Connection DOWN :" << conn->peerAddress().toIpPort().c_str();
         }
     }
 
-    // 可读写事件回调
+    // Readable/Writeable event callback
     void onMessage(const TcpConnectionPtr &conn, Buffer *buf, Timestamp time)
     {
         std::string msg = buf->retrieveAllAsString();
         conn->send(msg);
-        // conn->shutdown();   // 关闭写端 底层响应EPOLLHUP => 执行closeCallback_
     }
     TcpServer server_;
     EventLoop *loop_;
-
 };
 AsyncLogging* g_asyncLog = NULL;
 AsyncLogging * getAsyncLog(){
