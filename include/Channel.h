74596ebe0e406ba489bf61549fe826e4a236de11
Rain0832 <2710007824@qq.com>
2025-08-09 14:28:00 +0800

    docs: Change the comment style

--- include/Channel.h
+++ include/Channel.h
@@ -8,43 +8,63 @@
 
 class EventLoop;
 
-/**
- * 理清楚 EventLoop、Channel、Poller之间的关系  Reactor模型上对应多路事件分发器
- * Channel理解为通道 封装了sockfd和其感兴趣的event 如EPOLLIN、EPOLLOUT事件 还绑定了poller返回的具体事件
- **/
+// One Channel encapsulates: socketfd, event(like EPOLLIN, EPOLLOUT...),
+// It also binds the specific event returned by the poller.
 class Channel : noncopyable
 {
 public:
-    using EventCallback = std::function<void()>; // muduo仍使用typedef
+    using EventCallback = std::function<void()>;
     using ReadEventCallback = std::function<void(Timestamp)>;
 
     Channel(EventLoop *loop, int fd);
     ~Channel();
 
-    // fd得到Poller通知以后 处理事件 handleEvent在EventLoop::loop()中调用
+    // fd get notified by poller, handle a event
+    // It will be called in EventLoop::loop()
     void handleEvent(Timestamp receiveTime);
 
-    // 设置回调函数对象
+    // Set callbacks objects
     void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
     void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
     void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
     void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
 
-    // 防止当channel被手动remove掉 channel还在执行回调操作
+    // In case of channel is removed manually
+    // when channel still executing callback operations
     void tie(const std::shared_ptr<void> &);
 
     int fd() const { return fd_; }
     int events() const { return events_; }
     void set_revents(int revt) { revents_ = revt; }
 
-    // 设置fd相应的事件状态 相当于epoll_ctl add delete
-    void enableReading() { events_ |= kReadEvent; update(); }
-    void disableReading() { events_ &= ~kReadEvent; update(); }
-    void enableWriting() { events_ |= kWriteEvent; update(); }
-    void disableWriting() { events_ &= ~kWriteEvent; update(); }
-    void disableAll() { events_ = kNoneEvent; update(); }
+    // Set fd event status, which is equivalent to epoll_ctl add delete
+    void enableReading()
+    {
+        events_ |= kReadEvent;
+        update();
+    }
+    void disableReading()
+    {
+        events_ &= ~kReadEvent;
+        update();
+    }
+    void enableWriting()
+    {
+        events_ |= kWriteEvent;
+        update();
+    }
+    void disableWriting()
+    {
+        events_ &= ~kWriteEvent;
+        update();
+    }
+    void disableAll()
+    {
+        events_ = kNoneEvent;
+        update();
+    }
 
-    // 返回fd当前的事件状态
+    // Return current event status of fd
     bool isNoneEvent() const { return events_ == kNoneEvent; }
     bool isWriting() const { return events_ & kWriteEvent; }
     bool isReading() const { return events_ & kReadEvent; }
@@ -55,8 +75,8 @@ public:
     // one loop per thread
     EventLoop *ownerLoop() { return loop_; }
     void remove();
-private:
 
+private:
     void update();
     void handleEventWithGuard(Timestamp receiveTime);
 
@@ -64,16 +84,17 @@ private:
     static const int kReadEvent;
     static const int kWriteEvent;
 
-    EventLoop *loop_; // 事件循环
-    const int fd_;    // fd，Poller监听的对象
-    int events_;      // 注册fd感兴趣的事件
-    int revents_;     // Poller返回的具体发生的事件
+    EventLoop *loop_;
+
+    const int fd_; // Poller listen to the fd object
+    int events_;   // Register fd event
+    int revents_;  // Poller return specific event
     int index_;
 
     std::weak_ptr<void> tie_;
     bool tied_;
 
-    // 因为channel通道里可获知fd最终发生的具体的事件events，所以它负责调用具体事件的回调操作
+    // Callback objects
     ReadEventCallback readCallback_;
     EventCallback writeCallback_;
     EventCallback closeCallback_;
