bae001b54a63e84265a9ada76ae7b7c1ab74dd84
Rain0832 <2710007824@qq.com>
2025-08-10 22:07:51 +0800

    docs: Fix casing and improve comment clarity in Noncopyable class

--- include/noncopyable.h
+++ include/noncopyable.h
@@ -1,15 +1,15 @@
 #pragma once
 
-// the inheritance classof noncopyable will make the derived class object
+// the inheritance class of Noncopyable will make the derived class object
 // can be constructed and destructed normally,
 // but cannot perform copy construction and assignment construction
-class noncopyable
+class Noncopyable
 {
 public:
-    noncopyable(const noncopyable &) = delete;
-    noncopyable &operator=(const noncopyable &) = delete; // or void operator=(const noncopyable &) = delete; to forbid the chaining assignment
+    Noncopyable(const Noncopyable &) = delete;
+    Noncopyable &operator=(const Noncopyable &) = delete; // or void operator=(const noncopyable &) = delete; to forbid the chaining assignment
 
 protected:
-    noncopyable() = default;
-    ~noncopyable() = default;
+    Noncopyable() = default;
+    ~Noncopyable() = default;
 };
