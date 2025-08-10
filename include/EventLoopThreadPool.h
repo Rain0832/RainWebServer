623d456591c61b9d1b7924b00c78de48c67741fe
Rain0832 <2710007824@qq.com>
2025-08-10 21:56:51 +0800

    docs: Fix include directives and improve comment clarity in EventLoopThreadPool

--- include/EventLoopThreadPool.h
+++ include/EventLoopThreadPool.h
@@ -1,16 +1,17 @@
 #pragma once
 
 #include <functional>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
 
-#include "noncopyable.h"
 #include "ConsistenHash.h"
+#include "Noncopyable.h"
+
 class EventLoop;
 class EventLoopThread;
 
-class EventLoopThreadPool : noncopyable
+class EventLoopThreadPool : Noncopyable
 {
 public:
     using ThreadInitCallback = std::function<void(EventLoop *)>;
@@ -22,21 +23,24 @@ public:
 
     void start(const ThreadInitCallback &cb = ThreadInitCallback());
 
-    // 如果工作在多线程中，baseLoop_(mainLoop)会默认以轮询的方式分配Channel给subLoop
-    EventLoop *getNextLoop(const std::string& key);
+    // If work in multi-threads, baseLoop_ (mainLoop) will distribute Channel to subLoop by poll
+    EventLoop *getNextLoop(const std::string &key);
 
     std::vector<EventLoop *> getAllLoops(); // 获取所有的EventLoop
 
-    bool started() const { return started_; } // 是否已经启动
-    const std::string name() const { return name_; } // 获取名字
+    // Check if this thread pool has started
+    bool started() const { return started_; }
+
+    // Get name of this thread pool
+    const std::string name() const { return name_; }
 
 private:
-    EventLoop *baseLoop_; // 用户使用muduo创建的loop 如果线程数为1 那直接使用用户创建的loop 否则创建多EventLoop
-    std::string name_;//线程池名称，通常由用户指定，线程池中EventLoopThread名称依赖于线程池名称。
-    bool started_;//是否已经启动标志
-    int numThreads_;//线程池中线程的数量
-    int next_; // 新连接到来，所选择EventLoop的索引
-    std::vector<std::unique_ptr<EventLoopThread>> threads_;//IO线程的列表
-    std::vector<EventLoop *> loops_;//线程池中EventLoop的列表，指向的是EVentLoopThread线程函数创建的EventLoop对象。
-    ConsistentHash hash_; // 一致性哈希对象
+    EventLoop *baseLoop_;                                   // User use muduo create loop if thread number == 1, use user-created loop. else create multi-EventLoop
+    std::string name_;                                      // TreadPool name, defined by user, EventLoopThread name depend on thread pool name
+    bool started_;                                          // Is thread started
+    int numThreads_;                                        // Thread number in thread pool
+    int next_;                                              // New connection come, select EventLoop index
+    std::vector<std::unique_ptr<EventLoopThread>> threads_; // IO thread list
+    std::vector<EventLoop *> loops_;                        // Thread pool's EventLoop list, point to EventLoopThread function create EventLoop object
+    ConsistentHash hash_;                                   // Consistent Hash object
 };
