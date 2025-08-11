71c4c975a2141fb1d945b5fae648ef73d7a16b31
Rain0832 <2710007824@qq.com>
2025-08-11 21:42:28 +0800

    docs: Fix casing and improve comment clarity in Timer class

--- include/Timer.h
+++ include/Timer.h
@@ -1,15 +1,12 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include "noncopyable.h"
-#include "Timestamp.h"
 #include <functional>
 
-/**
- * Timer用于描述一个定时器
- * 定时器回调函数，下一次超时时刻，重复定时器的时间间隔等
- */
-class Timer : noncopyable
+#include "Timestamp.h"
+#include "Noncopyable.h"
+
+class Timer : Noncopyable
 {
 public:
     using TimerCallback = std::function<void()>;
@@ -18,26 +15,25 @@ public:
         : callback_(move(cb)),
           expiration_(when),
           interval_(interval),
-          repeat_(interval > 0.0) // 一次性定时器设置为0
+          repeat_(interval > 0.0)
     {
     }
 
-    void run() const 
-    { 
-        callback_(); 
+    void run() const
+    {
+        callback_();
     }
 
-    Timestamp expiration() const  { return expiration_; }
+    Timestamp expiration() const { return expiration_; }
     bool repeat() const { return repeat_; }
 
-    // 重启定时器(如果是非重复事件则到期时间置为0)
     void restart(Timestamp now);
 
 private:
-    const TimerCallback callback_;  // 定时器回调函数
-    Timestamp expiration_;          // 下一次的超时时刻
-    const double interval_;         // 超时时间间隔，如果是一次性定时器，该值为0
-    const bool repeat_;             // 是否重复(false 表示是一次性定时器)
+    const TimerCallback callback_; // Timer call back function
+    Timestamp expiration_;         // The next expiration time
+    const double interval_;        // Time out interval, if one-time timer, is 0
+    const bool repeat_;            // Whether it is repeated (false means one-time timer)
 };
 
 #endif // TIMER_H
