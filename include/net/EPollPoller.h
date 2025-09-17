cb50f5f0eb8d06ba5c8cfb704889cf2c9ff95136
Rain0832 <2710007824@qq.com>
2025-09-17 21:30:36 +0800

    docs: Update comment

--- include/net/EPollPoller.h
+++ include/net/EPollPoller.h
@@ -23,25 +23,25 @@ public:
     /**
      * @brief Poll IO events, return the active channels
      * @details 1. Call epoll_wait() to wait IO events
-     * 2. Handle the return values: numEvents
-     * 3. Use LOG_DEBUG and LOG_ERROR to record the debug and error information
-     * 4. Call fillActiveChannels() put active channels into activeChannels
-     * 5. Return the current timestamp
+     *          2. Handle the return values: numEvents
+     *          3. Use LOG_DEBUG and LOG_ERROR to record the debug and error information
+     *          4. Call fillActiveChannels() put active channels into activeChannels
+     *          5. Return the current timestamp
      */
     Timestamp poll(int timeoutMs, ChannelList *activeChannels) override;
 
     /**
      * @brief Update the channel
      * @details 1. Record channel fd, events, and index
-     * 2. Handle new channel(add to map) or delete channel
-     * 3. Handle registered channel(mod)
+     *          2. Handle new channel(add to map) or delete channel
+     *          3. Handle registered channel(mod)
      */
     void updateChannel(Channel *channel) override;
 
     /**
      * @brief Remove the channel
      * @details 1. Erase the channel from the map
-     * 2. Call update() EPOLL_CTL_DEL signal to delete the channel
+     *          2. Call update() EPOLL_CTL_DEL signal to delete the channel
      */
     void removeChannel(Channel *channel) override;
 
@@ -51,7 +51,7 @@ private:
     /**
      * @brief Fill active channels
      * @details 1. Set numEvents to the channel
-     * 2. Add the channel to activeChannels
+     *          2. Add the channel to activeChannels
      * @note Use const keyword to avoid modifying the member variables
      */
     void fillActiveChannels(int numEvents, ChannelList *activeChannels) const;
@@ -59,7 +59,7 @@ private:
     /**
      * @brief Update epoll_event(add/mod/del)
      * @details 1. A more base function to update, actually we use a fd to update the epoll_event
-     * 2. Can add/mod/del a channel
+     *          2. Can add/mod/del a channel
      */
     void update(int operation, Channel *channel);
 
